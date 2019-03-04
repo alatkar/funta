2843f2352b1c13ae32d96066a249e7a211e4f7ac
alatkar <alatkar@yahoo.com>
2019-03-03 18:50:21 -0800

    Missing file

---
+++ backend/Services/MainService/Contracts/Register.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace MainService.Contracts
+{
+    [JsonObject]
+    public class Register
+    {
+        [JsonProperty(PropertyName = "userName", Required = Required.Always)]
+        public string UserName { get; set; }
+
+        [JsonProperty(PropertyName = "email", Required = Required.Always)]
+        public virtual string Email { get; set; }
+
+        [JsonProperty(PropertyName = "password", Required = Required.Always)]
+        public virtual string PasswordHash { get; set; }
+
+        [JsonProperty(PropertyName = "phoneNumber")]
+        public virtual string PhoneNumber { get; set; }
+
+        [JsonProperty(PropertyName = "zipCode", Required = Required.Always)]
+        public string ZipCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
